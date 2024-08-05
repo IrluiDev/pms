e849d86908c0de0f79f8ab3d91c90f4a213a2cdb
irluidev <iramirez.spain@gmail.com>
2024-08-05 14:23:04 +0200

    [FIX]pms_hr_property: module test
    [UPD]pms_hr_property: update structure DB

--- pms_hr_property/__manifest__.py
+++ pms_hr_property/__manifest__.py
@@ -5,7 +5,11 @@
     "name": "PMS Hr Property",
     "summary": """
         Adds to the employee the property on which he works.""",
+<<<<<<< HEAD
     "version": "14.0.1.1.0",
+=======
+    "version": "14.0.2.0.0",
+>>>>>>> [FIX]pms_hr_property: module test
     "license": "AGPL-3",
     "author": "OsoTranquilo,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pms",
