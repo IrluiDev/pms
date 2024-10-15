9ddea7c43c278373c451a2b2bc9d9bf052b91dc2
irluidev <iramirez.spain@gmail.com>
2024-10-15 19:49:28 +0200

    [FIX] manifest

--- pms_hr_property/__manifest__.py
+++ pms_hr_property/__manifest__.py
@@ -5,15 +5,7 @@
     "name": "PMS Hr Property",
     "summary": """
         Adds to the employee the property on which he works.""",
-<<<<<<< HEAD
-<<<<<<< HEAD
     "version": "14.0.1.1.0",
-=======
-    "version": "14.0.2.0.0",
->>>>>>> [FIX]pms_hr_property: module test
-=======
-    "version": "14.0.1.0.0",
->>>>>>> [FIX]:hr_pms_property: remove class method, DB structure and updated test module
     "license": "AGPL-3",
     "author": "OsoTranquilo,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pms",
